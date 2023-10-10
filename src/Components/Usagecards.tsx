f8da4fbd6b6a71475ca265f66e727d797a7f3f55
PRANJALRANA11 <pranajlrana1235@gmail.com>
2023-10-10 18:15:44 +0530

    navbar-component-ui
    Signed-off-by: PRANJALRANA11 <pranajlrana1235@gmail.com>

---
+++ src/Components/Usagecards.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Usagecards = () => {
+  return (
+    <div className='flex mt-10'>
+      <div className='card1  w-80 h-40 bg-slate-800 ml-48 mr-10 border rounded-lg shadow-lg shadow-gray-900'>
+        <p></p>
+          hello
+      </div>
+      <div className='card2  w-80 h-40 bg-slate-800 mr-10 border rounded-lg shadow-gray-900'>
+          hello
+      </div>
+      <div className='card3  w-80 h-40 bg-slate-800 mr-10 border rounded-lg shadow-gray-900'>
+          hello
+      </div>
+      </div>
+  )
+}
+
+export default Usagecards;
