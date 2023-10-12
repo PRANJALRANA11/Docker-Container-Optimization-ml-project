d8f9d72513dc6b71d6c43344555f93e0207e84df
PRANJALRANA11 <pranajlrana1235@gmail.com>
2023-10-12 17:04:45 +0530

    completing-the-ui
    Signed-off-by: PRANJALRANA11 <pranajlrana1235@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Usagecards from './Components/Usagecards';
+import Containers from './Components/Containers';
 
 function App() {
   return (
     <div className=" App">
        <Navbar/>
        <Usagecards/>
+       <Containers/>
     </div>
   );
 }
--- src/Components/Usagecards.tsx
+++ src/Components/Usagecards.tsx
@@ -3,15 +3,32 @@ import React from 'react'
 const Usagecards = () => {
   return (
     <div className='flex mt-10'>
-      <div className='card1  w-80 h-40 bg-slate-800 ml-48 mr-10 border rounded-lg shadow-lg shadow-gray-900'>
-        <p></p>
-          hello
+      <div className='card1  w-96 h-40 bg-slate-800 ml-48 mr-10 border-black rounded-lg p-5'>
+        <p className='text-slate-400'>
+          CPU Resource Usage
+        </p>
+        <p className='text-white mt-10'>
+          Used <span className='text-green-500 text-3xl ml-44'>87%</span><span>/100%</span>
+        </p>
+        <div className='w-80 h-1 bg-slate-300 mt-3 rounded-md'></div>
       </div>
-      <div className='card2  w-80 h-40 bg-slate-800 mr-10 border rounded-lg shadow-gray-900'>
-          hello
+      <div className='card2  w-96 h-40 bg-slate-800 mr-10 border-black rounded-lg p-5'>
+        <p className='text-slate-400 font-poppins'>
+          Memory Usage
+        </p>
+        <p className='text-white mt-10'>
+          Used<span className='text-green-500 text-3xl ml-44'>87%</span><span>/100%</span>
+        </p>
+        <div className='w-80 h-1 bg-slate-300 mt-3 rounded-md'></div>
       </div>
-      <div className='card3  w-80 h-40 bg-slate-800 mr-10 border rounded-lg shadow-gray-900'>
-          hello
+      <div className='card3  w-96 h-40 bg-slate-800 mr-10 border-black rounded-lg p-5'>
+        <p className='text-slate-400'>
+          CPU Resource Usage
+        </p>
+        <p className='text-white mt-10'>
+          Used<span className='text-green-500 text-3xl ml-44'>87%</span><span>/100%</span>
+        </p>
+        <div className='w-80 h-1 bg-slate-300 mt-3 rounded-md'></div>
       </div>
       </div>
   )
